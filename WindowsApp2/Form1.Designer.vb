32afd6a836ae383d8a1560d85e6924aad554a70e
Brian Hallberg <brian@pahallbergs.net>
2018-08-02 00:38:38 -0400

    Updated with working code to read the actual data files

--- WindowsApp2/Form1.Designer.vb
+++ WindowsApp2/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,211 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblDatab = New System.Windows.Forms.Label()
+        Me.txtDBName = New System.Windows.Forms.TextBox()
+        Me.btnOpenDB = New System.Windows.Forms.Button()
+        Me.OpenFileDialogDB = New System.Windows.Forms.OpenFileDialog()
+        Me.lblStart = New System.Windows.Forms.Label()
+        Me.txtStart = New System.Windows.Forms.TextBox()
+        Me.txtEnd = New System.Windows.Forms.TextBox()
+        Me.lblEnd = New System.Windows.Forms.Label()
+        Me.txtNumberPY = New System.Windows.Forms.TextBox()
+        Me.lblNumberPY = New System.Windows.Forms.Label()
+        Me.lbFields = New System.Windows.Forms.ListBox()
+        Me.lblFields = New System.Windows.Forms.Label()
+        Me.BtnCopyData = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Year = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnOK
+        '
+        Me.btnOK.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOK.Location = New System.Drawing.Point(45, 399)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 0
+        Me.btnOK.Text = "&OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(481, 399)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblDatab
+        '
+        Me.lblDatab.AutoSize = True
+        Me.lblDatab.Location = New System.Drawing.Point(165, 9)
+        Me.lblDatab.Name = "lblDatab"
+        Me.lblDatab.Size = New System.Drawing.Size(57, 13)
+        Me.lblDatab.TabIndex = 2
+        Me.lblDatab.Text = "Data Base"
+        '
+        'txtDBName
+        '
+        Me.txtDBName.Location = New System.Drawing.Point(122, 22)
+        Me.txtDBName.Name = "txtDBName"
+        Me.txtDBName.Size = New System.Drawing.Size(454, 20)
+        Me.txtDBName.TabIndex = 3
+        '
+        'btnOpenDB
+        '
+        Me.btnOpenDB.Location = New System.Drawing.Point(28, 22)
+        Me.btnOpenDB.Name = "btnOpenDB"
+        Me.btnOpenDB.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpenDB.TabIndex = 4
+        Me.btnOpenDB.Text = "Open"
+        Me.btnOpenDB.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialogDB
+        '
+        Me.OpenFileDialogDB.FileName = "OpenFileDialogDB"
+        '
+        'lblStart
+        '
+        Me.lblStart.AutoSize = True
+        Me.lblStart.Location = New System.Drawing.Point(25, 57)
+        Me.lblStart.Name = "lblStart"
+        Me.lblStart.Size = New System.Drawing.Size(29, 13)
+        Me.lblStart.TabIndex = 6
+        Me.lblStart.Text = "Start"
+        '
+        'txtStart
+        '
+        Me.txtStart.Location = New System.Drawing.Point(60, 54)
+        Me.txtStart.Name = "txtStart"
+        Me.txtStart.Size = New System.Drawing.Size(100, 20)
+        Me.txtStart.TabIndex = 7
+        '
+        'txtEnd
+        '
+        Me.txtEnd.Location = New System.Drawing.Point(227, 54)
+        Me.txtEnd.Name = "txtEnd"
+        Me.txtEnd.Size = New System.Drawing.Size(100, 20)
+        Me.txtEnd.TabIndex = 9
+        '
+        'lblEnd
+        '
+        Me.lblEnd.AutoSize = True
+        Me.lblEnd.Location = New System.Drawing.Point(192, 57)
+        Me.lblEnd.Name = "lblEnd"
+        Me.lblEnd.Size = New System.Drawing.Size(26, 13)
+        Me.lblEnd.TabIndex = 8
+        Me.lblEnd.Text = "End"
+        '
+        'txtNumberPY
+        '
+        Me.txtNumberPY.Location = New System.Drawing.Point(456, 54)
+        Me.txtNumberPY.Name = "txtNumberPY"
+        Me.txtNumberPY.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumberPY.TabIndex = 11
+        '
+        'lblNumberPY
+        '
+        Me.lblNumberPY.AutoSize = True
+        Me.lblNumberPY.Location = New System.Drawing.Point(363, 57)
+        Me.lblNumberPY.Name = "lblNumberPY"
+        Me.lblNumberPY.Size = New System.Drawing.Size(87, 13)
+        Me.lblNumberPY.TabIndex = 10
+        Me.lblNumberPY.Text = "Number per Year"
+        '
+        'lbFields
+        '
+        Me.lbFields.FormattingEnabled = True
+        Me.lbFields.Location = New System.Drawing.Point(28, 109)
+        Me.lbFields.Name = "lbFields"
+        Me.lbFields.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lbFields.Size = New System.Drawing.Size(548, 95)
+        Me.lbFields.TabIndex = 12
+        '
+        'lblFields
+        '
+        Me.lblFields.AutoSize = True
+        Me.lblFields.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFields.Location = New System.Drawing.Point(25, 93)
+        Me.lblFields.Name = "lblFields"
+        Me.lblFields.Size = New System.Drawing.Size(95, 13)
+        Me.lblFields.TabIndex = 13
+        Me.lblFields.Text = "Fields to Export"
+        '
+        'BtnCopyData
+        '
+        Me.BtnCopyData.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnCopyData.Location = New System.Drawing.Point(252, 399)
+        Me.BtnCopyData.Name = "BtnCopyData"
+        Me.BtnCopyData.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCopyData.TabIndex = 14
+        Me.BtnCopyData.Text = "&Copy Data"
+        Me.BtnCopyData.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Year})
+        Me.DataGridView1.Location = New System.Drawing.Point(28, 231)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(548, 150)
+        Me.DataGridView1.TabIndex = 15
+        '
+        'Year
+        '
+        Me.Year.HeaderText = "Year"
+        Me.Year.Name = "Year"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(588, 434)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.BtnCopyData)
+        Me.Controls.Add(Me.lblFields)
+        Me.Controls.Add(Me.lbFields)
+        Me.Controls.Add(Me.txtNumberPY)
+        Me.Controls.Add(Me.lblNumberPY)
+        Me.Controls.Add(Me.txtEnd)
+        Me.Controls.Add(Me.lblEnd)
+        Me.Controls.Add(Me.txtStart)
+        Me.Controls.Add(Me.lblStart)
+        Me.Controls.Add(Me.btnOpenDB)
+        Me.Controls.Add(Me.txtDBName)
+        Me.Controls.Add(Me.lblDatab)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnOK)
+        Me.Name = "Form1"
+        Me.Text = "MCH Database Dump"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblDatab As Label
+    Friend WithEvents txtDBName As TextBox
+    Friend WithEvents btnOpenDB As Button
+    Friend WithEvents OpenFileDialogDB As OpenFileDialog
+    Friend WithEvents lblStart As Label
+    Friend WithEvents txtStart As TextBox
+    Friend WithEvents txtEnd As TextBox
+    Friend WithEvents lblEnd As Label
+    Friend WithEvents txtNumberPY As TextBox
+    Friend WithEvents lblNumberPY As Label
+    Friend WithEvents lbFields As ListBox
+    Friend WithEvents lblFields As Label
+    Friend WithEvents BtnCopyData As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Year As DataGridViewTextBoxColumn
 End Class
--- WindowsApp2/Form1.vb
+++ WindowsApp2/Form1.vb
@@ -1,3 +1,297 @@
-﻿Public Class Form1
+﻿Option Explicit On
+Imports System.Runtime.InteropServices
+Imports System
+Imports System.IO
 
+Public Class Form1
+    Private FieldNameFile As String
+    Private FieldDescFile As String
+    Private TempStr As String
+    Private StartYear As Integer
+    Private EndYear As Integer
+    Private InYear As Integer
+    Private Observations As Integer
+    Private NumRecords As Integer
+    Private Vlsperrec As Integer = 20
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BtnOpenDB_Click(sender As Object, e As EventArgs) Handles btnOpenDB.Click
+        Dim strText As String
+        OpenFileDialogDB.Title = "Open Database file"
+        OpenFileDialogDB.InitialDirectory = "."
+        OpenFileDialogDB.Filter = "Data Files (*.dat)|*.dat"
+        If OpenFileDialogDB.ShowDialog() =
+            System.Windows.Forms.DialogResult.OK Then
+
+            Try
+                strText = OpenFileDialogDB.FileName
+                txtDBName.Text = strText
+            Catch ex As Exception
+                MessageBox.Show("Error Getting File", "Error")
+            End Try
+            '
+            '  If there was data on the form, delete it
+            '
+            txtEnd.Text = " "
+            txtNumberPY.Text = " "
+            txtStart.Text = " "
+
+            For i As Integer = lbFields.Items.Count - 1 To 0 Step -1
+                lbFields.Items.RemoveAt(i)
+            Next
+        End If
+
+    End Sub
+
+    Private Sub BtnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        Dim strIdx As Integer
+        Dim line As String = ""
+        Dim lined As String = ""
+        Dim dline As String = ""
+
+        'IOBS = IBASYR2 - IBASYR1 + 1
+        'ICENTURY = 100 * Int(IBASYR1 / 100)
+        'IOBS = IOBS * IPERIOD
+        'IRECORD = Int(IOBS / 20)
+        'If IRECORD * 20 < IOBS Then IRECORD = IRECORD + 1
+
+        TempStr = txtDBName.Text
+        TempStr = Trim(TempStr)
+
+        strIdx = InStr(1, TempStr, ".DAT")
+        FieldNameFile = Microsoft.VisualBasic.Left(TempStr, strIdx) + "nme"
+        FieldDescFile = Microsoft.VisualBasic.Left(TempStr, strIdx) + "dsc"
+
+        Try
+            ' Create an instance of StreamReader to read from a file.
+            Dim sr As StreamReader = New StreamReader(FieldNameFile)
+            ' Read and display the lines from the file until the end 
+            ' of the file is reached.
+            line = sr.ReadToEnd()
+            sr.Close()
+        Catch Ex As Exception
+            ' Let the user know what went wrong.
+            Console.WriteLine("The file could not be read:")
+            Console.WriteLine(Ex.Message)
+        End Try
+
+        Try
+            ' Create an instance of StreamReader to read from a file.
+            Dim sr As StreamReader = New StreamReader(FieldDescFile)
+            ' Read and display the lines from the file until the end 
+            ' of the file is reached.
+            lined = sr.ReadToEnd()
+            sr.Close()
+        Catch Ex As Exception
+            ' Let the user know what went wrong.
+            Console.WriteLine("The file could not be read:")
+            Console.WriteLine(Ex.Message)
+        End Try
+
+        For i As Integer = 1 To Len(line) Step 8
+            TempStr = Trim(Mid(line, i, 8))
+            If i = 1 Then
+                StartYear = Microsoft.VisualBasic.Left(TempStr, 4)
+                txtStart.Text = Microsoft.VisualBasic.Left(TempStr, 4)
+                EndYear = Microsoft.VisualBasic.Right(TempStr, 4)
+                txtEnd.Text = Microsoft.VisualBasic.Right(TempStr, 4)
+            End If
+            If i = 9 Then
+                InYear = TempStr
+                txtNumberPY.Text = TempStr
+            End If
+            'Debug.Write(TempStr & Environment.NewLine)
+            If i > 9 Then
+                TempStr = TempStr & " - " & Trim(Mid(lined, ((i - 17) / 8 * 80) + 1, 80))
+                lbFields.Items.Add(TempStr)
+            End If
+        Next
+
+        '  The data file will have Observations for each variable
+        '    This is the number of years of observations * the number of them per year
+        '    Data can be Yearly, Quarterly, or Monthly
+        '  The data file puts the observations into records with 20 Observations per Record
+        Observations = (EndYear - StartYear + 1) * InYear
+        NumRecords = Int(Observations / Vlsperrec)
+        If NumRecords * Vlsperrec < Observations Then
+            NumRecords = NumRecords + 1
+        End If
+
+        For i As Integer = StartYear To EndYear
+            DataGridView1.Rows.Add(Convert.ToString(i))
+        Next
+
+    End Sub
+
+    Private Declare Sub CopyMemory Lib "kernel32" Alias "RtlMoveMemory" _
+        (ByRef hpvDest As Byte, ByVal hpvSource As String, ByVal cbCopy As Integer)
+
+    Private Declare Sub CopyMemory1 Lib "kernel32" Alias "RtlMoveMemory" _
+        (ByRef hpvDest As Single, ByRef hpvSource As Byte, ByVal cbCopy As Integer)
+
+    ' Substitutes for the old CV* and MK* functions
+    ' from qbasic.
+    '
+    ' Calling CV* functions with a string of the wrong length
+    ' (ie. calling CVL with a 8 byte string instead of a 4
+    '  byte string) will cause an error.
+    '
+    '
+    '
+    ' CVSMBF, CVDMBF, MKSMBF and MKDMBF Functions
+    ' -------------------------------------------
+    '
+    ' Microsoft Binary Format <-> IEEE floating point functions
+    ' were converted from C source found on borland's ftp site.
+    '
+    ' These routines (CVSMBF, CVDMBF, MKSMBF, MKDMBF) do not
+    ' handle IEEE NAN's and infinites.  IEEE denormals are
+    ' treated as 0's.
+    '
+    ' Conversion of doubles can cause overflows when the numbers
+    ' simply cannot be converted.  See the code and comments
+    ' for more details.
+    '
+    '
+    ' Single Precision Format:
+    '    MS Binary Format:
+    '    byte order =>    m3  |  m2  | m1 | exponent
+    '    m1 is the most significant byte => sbbb|bbbb
+    '    m3 is the least significant byte
+    '          m = mantissa byte
+    '          s = sign bit
+    '          b = bit
+    '
+    '    IEEE Format:
+    '        m3        m2        m1    exponent
+    '    mmmm|mmmm mmmm|mmmm emmm|mmmm seee|eeee
+    '          s = sign mit
+    '          e = exponent bit
+    '          m = mantissa bit
+    '
+    '
+    ' Double Precision Format:
+    '    MS Binary Format:
+    '    byte order => m7 | m6 | m5 | m4 | m3 | m2 | m1 | exponent
+    '    m1 is the most significant byte => smmm|mmmm
+    '    m7 is the least significant byte
+    '          m = mantissa byte
+    '          s = sign bit
+    '          b = bit
+    '
+    '    IEEE Format:
+    '     byte 8    byte 7    byte 6    byte 5    byte 4   and so on
+    '    seee|eeee eeee|mmmm mmmm|mmmm mmmm|mmmm mmmm|mmmm ....
+    '          m = mantissa byte
+    '          s = sign bit
+    '          b = bit
+    '
+    Public Function CVSMBF(ByVal s As String) As Single
+        Dim i As Integer
+        Dim msbin(4) As Byte
+        Dim ieee(4) As Byte
+        Dim ieee_exp As Byte
+        Dim sng As Single
+
+        If Len(s) <> 4 Then
+            MsgBox("Invalid string in CVSMBF.")
+            CVSMBF = 0
+            Exit Function
+        End If
+
+        'Debug.WriteLine(Convert.ToString(Asc(Mid(s, 4, 1)), 16) & " " & Convert.ToString(Asc(Mid(s, 3, 1)), 16) & " " & Convert.ToString(Asc(Mid(s, 2, 1)), 16) & " " & Convert.ToString(Asc(Mid(s, 1, 1)), 16))
+
+        ' copy s and to msbin
+        Call CopyMemory(msbin(0), s, 4)
+
+        'Debug.WriteLine(Convert.ToString(msbin(3), 16) & " " & Convert.ToString(msbin(2), 16) & " " & Convert.ToString(msbin(1), 16) & " " & Convert.ToString(msbin(0), 16))
+
+        ' Any msbin with an exponent of zero = 0
+        If msbin(3) = 0 Then
+            CVSMBF = 0
+            Exit Function
+        End If
+
+        ieee(3) = msbin(2) And &H80
+
+        ' MBF is bias 128 and IEEE is bias 127.  Also, MBF places
+        ' the decimal point before the assumed bit, while IEEE
+        ' places the decimal point after the assumed bit.
+        ieee_exp = msbin(3) - 2   ' Actually ms_bin(3)-1-128+127
+
+        ' The first 7 bits of the exponent in ieee(3)
+        ieee(3) = ieee(3) Or (ieee_exp \ 2)
+
+        ' The one remaining bit in the first bit of ieee(2)
+        ieee(2) = ieee(2) Or ((ieee_exp And 1) * 128)
+
+        ' mask out the msbin sign bit
+        ieee(2) = ieee(2) Or (msbin(2) And &H7F)
+
+        ieee(1) = msbin(1)
+        ieee(0) = msbin(0)
+
+        'Debug.WriteLine(Convert.ToString(ieee(3), 16) & " " & Convert.ToString(ieee(2), 16) & " " & Convert.ToString(ieee(1), 16) & " " & Convert.ToString(ieee(0), 16))
+
+        'Copy ieee to sng
+        Call CopyMemory1(sng, ieee(0), 4)
+
+        CVSMBF = sng
+    End Function
+
+    Private Sub BtnCopyData_Click(sender As Object, e As EventArgs) Handles BtnCopyData.Click
+        Dim bts() As Byte
+        Dim theObs As Integer
+        Dim x As Integer
+        Dim TempStrArry() As String
+        Dim lstCol As Integer = 0
+
+        Try
+            ' Create an instance of StreamReader to read from a file.
+            bts = My.Computer.FileSystem.ReadAllBytes(txtDBName.Text)
+            'dline = My.Computer.FileSystem.ReadAllText(txtDBName.Text, System.Text.Encoding.UTF32)
+            'Dim sr As StreamReader = New StreamReader(txtDBName.Text)
+            ' Read and display the lines from the file until the end 
+            ' of the file is reached.
+            'line = sr.ReadToEnd()
+            'sr.Close()
+        Catch Ex As Exception
+            ' Let the user know what went wrong.
+            Console.WriteLine("The file could not be read:")
+            Console.WriteLine(Ex.Message)
+        End Try
+
+        For Each x In lbFields.SelectedIndices
+            '  Get the column name no description from the selected variable
+            '    also add a column to the data grid for the values
+            TempStr = lbFields.Items.Item(x)
+            TempStrArry = Split(TempStr)
+            TempStr = TempStrArry(0)
+            DataGridView1.Columns.Add(TempStr, TempStr)
+            lstCol = lstCol + 1
+            Dim offset As Integer = (Vlsperrec * NumRecords * x)
+            '
+            '  Now loop thru the data, convert it and storeit in the data grid
+            '
+            For i As Integer = 1 To Observations
+                theObs = (i - 1) * 4 + offset
+                TempStr = Chr(bts(theObs)) & Chr(bts(theObs + 1)) & Chr(bts(theObs + 2)) & Chr(bts(theObs + 3))
+                DataGridView1.Rows.Item(i - 1).Cells.Item(lstCol).Value = CVSMBF(TempStr)
+            Next i
+        Next x
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
 End Class
+
+
